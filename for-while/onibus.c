83593bf8a6ed0f169adae36856507ad6e5e229f5
narciso-roberto <narcisoroberto12@gmail.com>
2024-04-04 13:54:15 -0300

    Questoes

---
+++ for-while/onibus.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+int main(){
+    int tot=0,m=0,c=0;
+    
+    scanf("%d", &c);
+    
+    while(tot < c*2){
+        
+        scanf("%d", &m);
+        tot = tot + m;
+
+        if(tot >= c*2){
+            printf("hora de partir\n");
+        }else if(tot == 0){
+            printf("vazio\n");
+        }else if(tot<c){
+            printf("ainda cabe\n");
+        }else if(tot>=c){
+            printf("lotado\n");
+        }
+    }
+    
+}
