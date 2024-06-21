855407df576e87f6b53525779431f56cbdce739f
Narciso Roberto de Souza <narcisoroberto1369@alu.ufc.br>
2024-06-21 18:23:31 -0300

    Formulario de envio de dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,73 @@
-import React from 'react';
-import useFetch from './useFetch';
+import React, { useState } from 'react';
+
 
 const App = () => {
+const [dados,setDados] = useState(null)
+const [form, setForm] = useState(
+  {nome:'',email:'',senha:'',
+  CEP:'',rua:'',numero:'',
+  bairro:'',cidade:'',estado:''})
 
-  const {data,request,load,erro} = useFetch()
 
+  async function handleSubmit(event){
+    event.preventDefault()
+    setDados('carregando')
+    try{
+      await fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(form),
+      });
+      setDados('dados enviados')
+    }
+    catch{
+      setDados('Houve um erro *_*')
+    }
+  }
 
-  React.useEffect(() => {
-    request('https://ranekapi.origamid.dev/json/api/produto')
-  },[request])
+  function handleChange({target}){
+    const {id,value} = target
+    setForm({...form, [id] : value})
+  }
 
-  if(erro) return <div>{erro}</div>
-  if(load) return <div>carregando</div>
-  if(data)
-    return (
-      <ul>
-        {data.map((prod) => {
-          return <li key={prod.id}>{prod.nome}</li>
-        })}
-      </ul>
-    );
-  
+  return(<>
+  <form onSubmit={handleSubmit}>
+    <label htmlFor='nome'>Nome</label>
+    <input onChange={handleChange} name='nome' id='nome' type='text' value={form.nome}/>
+
+    <label htmlFor='email'>E-mail</label>
+    <input onChange={handleChange} name='email' id='email' type='email' value={form.email}/>
+
+    <label htmlFor='senha'>Senha</label>
+    <input onChange={handleChange} name='senha' id='senha' type='password' value={form.senha}/>
+
+    <label htmlFor='CEP'>CEP</label>
+    <input onChange={handleChange} name='CEP' id='CEP' type='text' value={form.CEP}/>
+
+    <label htmlFor='rua'>Rua</label>
+    <input onChange={handleChange} name='rua' id='rua' type='text' value={form.rua}/>
+
+    <label htmlFor='numero'>Numero</label>
+    <input onChange={handleChange} name='numero' id='numero' type='text' value={form.numero}/>
+
+    <label htmlFor='bairro'>Bairro</label>
+    <input onChange={handleChange} name='bairro' id='bairro' type='text' value={form.bairro}/>
+
+    <label htmlFor='cidade'>Cidade</label>
+    <input onChange={handleChange} name='cidade' id='cidade' type='text' value={form.cidade}/>
+
+    <label htmlFor='estado'>Estado</label>
+    <input onChange={handleChange} name='estado' id='estado' type='text' value={form.estado}/>
+
+    <button>Enviar</button>
+    <div>{dados != null ? dados: ''}</div>
+
+  </form>
+  </>)
 };
 
-export default App;
 
+
+export default App;
