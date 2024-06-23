70f54e78ba0cfcb76752a91e6d148e1007ab0387
Narciso Roberto de Souza <narcisoroberto1369@alu.ufc.br>
2024-06-23 17:51:45 -0300

    forms com react

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
+
 
 
 const App = () => {
-const [dados,setDados] = useState(null)
-const [form, setForm] = useState(
-  {nome:'',email:'',senha:'',
-  CEP:'',rua:'',numero:'',
-  bairro:'',cidade:'',estado:''})
 
+  const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+  const [state,setState] = React.useState([])
 
-  async function handleSubmit(event){
-    event.preventDefault()
-    setDados('carregando')
-    try{
-      await fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form),
-      });
-      setDados('dados enviados')
-    }
-    catch{
-      setDados('Houve um erro *_*')
+  const verif = (cor) => {
+    return state.includes(cor)
+  }
+
+  const handleChange = ({target}) => {
+    if(target.checked){
+      setState([...state,target.value])
+    }else{
+      setState(state.filter((cores) => cores != target.value))
     }
   }
 
-  function handleChange({target}){
-    const {id,value} = target
-    setForm({...form, [id] : value})
-  }
-
-  return(<>
-  <form onSubmit={handleSubmit}>
-    <label htmlFor='nome'>Nome</label>
-    <input onChange={handleChange} name='nome' id='nome' type='text' value={form.nome}/>
-
-    <label htmlFor='email'>E-mail</label>
-    <input onChange={handleChange} name='email' id='email' type='email' value={form.email}/>
-
-    <label htmlFor='senha'>Senha</label>
-    <input onChange={handleChange} name='senha' id='senha' type='password' value={form.senha}/>
-
-    <label htmlFor='CEP'>CEP</label>
-    <input onChange={handleChange} name='CEP' id='CEP' type='text' value={form.CEP}/>
-
-    <label htmlFor='rua'>Rua</label>
-    <input onChange={handleChange} name='rua' id='rua' type='text' value={form.rua}/>
-
-    <label htmlFor='numero'>Numero</label>
-    <input onChange={handleChange} name='numero' id='numero' type='text' value={form.numero}/>
-
-    <label htmlFor='bairro'>Bairro</label>
-    <input onChange={handleChange} name='bairro' id='bairro' type='text' value={form.bairro}/>
-
-    <label htmlFor='cidade'>Cidade</label>
-    <input onChange={handleChange} name='cidade' id='cidade' type='text' value={form.cidade}/>
-
-    <label htmlFor='estado'>Estado</label>
-    <input onChange={handleChange} name='estado' id='estado' type='text' value={form.estado}/>
-
-    <button>Enviar</button>
-    <div>{dados != null ? dados: ''}</div>
-
-  </form>
-  </>)
+  return (
+   <div>
+      {coresArray.map((cor) => {
+        return(<label key={cor}>
+          <input type="checkbox" onChange={handleChange} checked={verif(cor)} value={cor}/>{cor}
+        </label>)
+      })}
+      {state}
+   </div>
+  );
 };
+
 
 
 
